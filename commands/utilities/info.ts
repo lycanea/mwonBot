c3ba2315c86670ff5329706b9772689deb0472de
lycanea <lycanea@lycanea.dev>
2025-04-28 20:38:52 +1000

    bump version and add more info to the info command

--- commands/utilities/info.ts
+++ commands/utilities/info.ts
@@ -10,7 +10,7 @@ module.exports = {
 	async execute(interaction) {
 		const embed = new EmbedBuilder()
 			.setTitle('MwonBot!')
-            .setDescription('mwonbot is an [open source](https://github.com/lycanea/mwonBot) discord bot made by [lycanea](https://lycanea.dev) , it uses data collected by lycanea and various other players to provide useful commands for melon king.')
+            .setDescription('mwonbot is an [open source](https://github.com/lycanea/mwonBot) discord bot made by [lycanea](https://lycanea.dev) , it uses data collected by lycanea and various other players to provide useful commands for melon king.\nYou have express permission to use this bot in any server, host your own instance of it or use any data for your personal projects :3')
 			.addFields(
 				{ name: 'Main features', value: `- Currency Conversion (convert between gold, shards and cshards)\n- Information on Gift of Gold levels, trophies and housing levels (includes prices and effects)\n- Information on all enchantments and reforges`, inline: true }
 			)
